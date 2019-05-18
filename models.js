3fb65b3b1755e1e353f858908b18438000b584f6
Ryan Jasinski <jasinski.17@gmail.com>
2019-05-18 11:05:14 -0400

    DB created

--- models.js
+++ models.js
@@ -1,2 +1,38 @@
+const Sequelize = require("Sequelize");
 const db = new Sequelize("postgres://localhost:5432/plantr");
-module.exports(db);
+
+const gardener = db.define("gardener", {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  age: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  }
+});
+
+const plot = db.define("plot", {
+  size: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  shaded: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false
+  }
+});
+
+const vegetable = db.define("vegetable", {
+  name: {
+    type: Sequelize.STRING
+  },
+  color: {
+    type: Sequelize.STRING
+  },
+  planted_on: {
+    type: Sequelize.DATE
+  }
+});
+
+module.exports = db;
